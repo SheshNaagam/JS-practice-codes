b30614a6116679257dce3f26ce894ed8d401cd7a
NaagShaktee <astitvasinha1221@protonmail.com>
2021-03-23 02:46:51 +0530

    23Mar21

--- VanillaJS/Lesson 02: Variables/app.js
+++ VanillaJS/Lesson 02: Variables/app.js
@@ -8,36 +8,37 @@ let anyLetName = 'Variable let';   // let init and declaration
 console.log(anyLetName);
 
 const constName = 'Variable const';  // const init and declaration. Cannot Reassign value to const.
+console.log(constName);
 
 // Primitive Data Types
 console.log(">>>PRIMITIVE DATA TYPES")
 
-const named = 'String';  //String
+const named = 'Random Text';  //String
 const born = 1337;         //Number, Decimal
 const truee = false;       //Boolean
 const empty = null;        //Null {Returns Object>Bug}
 let undef;                 //Undefined
 const sym = Symbol();      //ES6 Primitive>Symbol
 
-console.log(named);
-console.log(born);
-console.log(truee);
-console.log(empty);
-console.log(undef);
-console.log(sym);
+console.log("String: ", named);
+console.log("Number: ", born);
+console.log("Boolean: ", truee);
+console.log("Null: ", empty);
+console.log("Undefined: ", undef);
+console.log("Symbol: ", sym);
 
 // Reference Data Types {ALWAYS RETURN OBJECT}
 console.log(">>>REFERENCE DATA TYPES");
 
 const barray = ['index1.value', 'index2.value', 'index3.value'];              //Array
-console.log(barray);
+console.log("Array: ", barray);
 
 const objectOne = {           //Object
   ID: 'ObjMember',
   Quantity: 30,
 }
-console.log(typeof objectOne);       
-console.log(objectOne.ID, objectOne.Quantity);
+console.log("Return Type for Object: ", typeof objectOne);       
+console.log("Object Members: ", objectOne.ID, objectOne.Quantity);
 
 const dateyear = new Date();  //Date
-console.log(dateyear);
+console.log("Date: ", dateyear);
