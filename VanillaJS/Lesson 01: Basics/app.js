35c244323a002699574eb619315729dd415ca7b2
NaagShaktee <astitvasinha1221@protonmail.com>
2021-03-22 11:59:43 +0530

    22mar21

---
+++ VanillaJS/Lesson 01: Basics/app.js
@@ -0,0 +1,24 @@
+console.log('Welcome To Console. Here You Can Run JS Locally But Code You Run Will Not Be Persistent.') //LOG OUTPUT TO CONSOLE
+  
+console.clear();           // Clear Console Screen
+console.log('123');        // String Log
+console.log(123);          // Numerical Log
+console.log([1,2,3]);      // Array Log
+console.log({a:1,b:2});    // Object Log
+console.table({a:1,b:2});  // Table Log
+console.error("ERROR!");   // Error Log
+console.warn("Warning!");  // Warning Log
+console.time("Timer");     // Timer
+  console.log("Instance 01");    // Code Block Executes
+  console.log("Instance 02");    // Code Block Executes
+  console.log("Instance 03");    // Code Block Executes
+  console.log("Instance 04");    // Code Block Executes
+  console.log("Instance 05");    // Code Block Executes
+  console.log("Instance 06");    // Code Block Executes
+  console.log("Instance 07");    // Code Block Executes
+console.timeEnd("Timer");        // Time Taken To Execute
+/*
+This is a multiline comment.
+Time & TimeEnd Function Calculates Time Taken
+To Execute The Block.
+*/
---
+++ VanillaJS/Lesson 02: Variables/app.js
@@ -0,0 +1,19 @@
+// 3 Types: var, let, const
+
+var anyName = 'Any Name';      // var init and declaration. Use $anyname when working with JQuery
+console.log(anyName);
+
+let anyLetName = 'Let Name';   // let init and declaration
+console.log(anyLetName);
+
+const constName = 'Constant Type';  // const init and declaration. Cannot Reassign value to const.
+
+// Object Declaration
+const objectOne = {
+
+  ID: 'Ginger',
+  Quantity: 30
+
+}
+console.log(objectOne);       
+console.log(objectOne.ID, objectOne.Quantity); 
