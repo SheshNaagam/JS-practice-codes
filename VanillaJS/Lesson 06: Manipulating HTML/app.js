1d6a5ddfd965483e56aad578a5062513b6b37d54
NaagShaktee <astitvasinha1221@protonmail.com>
2021-03-24 03:57:56 +0530

    24Mar21

--- VanillaJS/Lesson 06: Manipulating HTML/app.js
+++ VanillaJS/Lesson 06: Manipulating HTML/app.js
@@ -1,11 +1,14 @@
 // Manipulating HTML Dynamically
-
+var1 = "Beginner ";
+var2 = 2;
+var3 = " Advanced Soon!";
 //ES5 Style Implementation
-const insertHTML = "<ul type='square' style ='color:gold'>" +
+const insertHTML = "<ul type='square' style ='color:red'>" +
 "<li>1. This is called an InnerHTML.</li>" +
 "<li>2. You can Insert HTML into HTML Document Using InnerHTML.</li>" +
 "<li>3. Here It Is Implemented in ES5 Standards.</li>" +
 "<li>4. ES5 Makes It Difficult To Insert HTML With JS.</li>" +
+"<li>5. Random Variables Below</li><br>" + var1 + var2 + var3 +
 "<br><br>" +
 "</ul>";
 
@@ -13,10 +16,11 @@ document.getElementById('ES5').innerHTML += insertHTML;
 
 //ES6 Style Implementation WITH TEMPLATE LITERALS
 const innertHTML = `
-  <ul type="star"><strong><h3>
+  <ul type="disc" style = "color:red"><strong><h3>
     <li>1. This is another InnerHTML.</li>
     <li>2. Here It Is Implemented in ES6 Standards.</li>
-    <li>3. ES6 Makes It Very Easy To Manipulate HTML Through JS.</li></strong></h3>
+    <li>3. ES6 Makes It Very Easy To Manipulate HTML Through JS.</li>
+    <li>4. Random Variables Below</li><br>${var1} ${var2} ${var3}</strong></h3>
   </ul>
 `;
 
