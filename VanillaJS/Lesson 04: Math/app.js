32530997680511b1277d261785f516cf0ba35cdc
NaagShaktee <astitvasinha1221@protonmail.com>
2021-03-24 01:07:06 +0530

    24Mar21

--- VanillaJS/Lesson 04: Math/app.js
+++ VanillaJS/Lesson 04: Math/app.js
@@ -2,5 +2,17 @@
 console.log("Adding 1+2 : ", 1+2, "| Subtracting 2-1 : ", 2-1, "| Multiplying 1 x 2 : ", 1*2, "| Dividing 2/1 :", 2/1, "| Modulo 2%1 : ", 2%1);
 
 // Math Functions in JS
-let mathvar;
-
+// Math is an Object and Later is Property of Math. It is a Terminology of JS.
+console.log(">>> Math OBJECT and IT'S PROPERTIES");
+console.log("Property Pi: ", Math.PI);
+console.log("Property Euler's Number: ", Math.E);
+console.log("Property Square Root: ", Math.sqrt(9));
+console.log("Property Absolute: ", Math.abs(-21));
+console.log("Property Rounding Off 4.7: ", Math.round(4.7), "Property Rounding Off 4.3: ", Math.round(4.3));
+console.log("Property Flooring 4.7: ", Math.floor(4.7), "Property Ceiling 4.3", Math.ceil(4.3));
+console.log("Property 2 To The Power 10: ", Math.pow(2, 10));
+console.log("Property Minimum Valued Number: ", Math.min(1,2,3,4,5,6,-19));
+console.log("Property Maximum Valued Number", Math.max(1,2,3,4,5,6,-19));
+console.log("Property Random Number Generation from 0 to 10", Math.random()*11);
+console.log("Random Whole Number from 1-10: ", Math.floor(Math.random()*11));
+// Random will Generate 0 to 1. Multiplying it with a Number is a Small Trick To Make it a Range.
