a3625e182691c4042d53d26da3ab58f05cf91083
NaagShaktee <astitvasinha1221@protonmail.com>
2021-03-24 21:26:20 +0530

    24Mar21

--- VanillaJS/Lesson 08: Date Functions/app.js
+++ VanillaJS/Lesson 08: Date Functions/app.js
@@ -12,20 +12,32 @@ console.log(now);
 
 console.log("Year(Get): ", now.getFullYear());
 now.setFullYear(2021);
-console.log("Year(SET): ", now.getFullYear());  // FullYear
+console.log("Year(SET): ", now.getFullYear());     // FullYear
 
 console.log("Month(Get): ", now.getMonth());
 now.setMonth(1);
-console.log("Month(Set): ", now.getMonth());  // Month
+console.log("Month(Set): ", now.getMonth());       // Month
 
-console.log("Day(Get): ", now.getDay());  // No Set Function
+console.log("Day(Get): ", now.getDay());           // No Set Function
 
 console.log("Date(Get): ", now.getDate());
 now.setDate(30);
-console.log("Date(Set): ", now.getDate());    // Date
+console.log("Date(Set): ", now.getDate());         // Date
 
 console.log("Hour(Get): ", now.getHours());
 now.setHours(23);
-console.log("Hour(Set): ", now.getHours());   // Hours
+console.log("Hour(Set): ", now.getHours());        // Hours
 
-console.log("Minutes:(Get) ")
+console.log("Minutes(Get): ", now.getMinutes());
+now.setMinutes(59);
+console.log("Minutes(Set): ", now.getMinutes());   // Minutes
+
+console.log("Seconds(Get): ", now.getSeconds());
+now.setSeconds(49);
+console.log("Seconds(Set): ", now.getSeconds());   // Seconds
+
+console.log("Milliseconds(Get): ", now.getMilliseconds()); 
+now.setMilliseconds(22);
+console.log("Milliseconds(Set): ", now.getMilliseconds());  // Milliseconds
+
+console.log("Timestamp: ", now.getTime());   // Shows Timestamp For Time passed Since Date
