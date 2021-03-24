24e6e9c63594bf33fd421ca74a1627782a6f61f3
NaagShaktee <astitvasinha1221@protonmail.com>
2021-03-24 21:18:54 +0530

    24Mar21

--- VanillaJS/Lesson 07: Array Functions/app.js
+++ VanillaJS/Lesson 07: Array Functions/app.js
@@ -1,7 +1,7 @@
 // Array
 const Arr = [1, "String", {a:1, b:2}, true, null, new Date()];    // Array is Data Structure Comprising Multiple Data Types.
 console.log("Mixed Array: ", Arr, "Array Length: ", Arr.length);    // Length Property of Array
-const Arr2 = new Array(1,2,3,4,5,6,7,8,9);
+const Arr2 = new Array(2,3,4,5,6,7,8,9,10);
 console.log("New Array Initialisation: ", Arr2);    // Another Way to Initialise Array in JS.
 console.log("Array Value From 3rd Index: ", Arr2[2]);    // Array Stores Values in Indexes Starting with 0
 
@@ -22,10 +22,8 @@ Arr2.splice(4, 7);   // Same As Slicing String, Last Argument is Exclusive meani
 console.log("Splicing of Array: ", Arr2);
 console.log("Reversed Array: ", Arr2.reverse());   // Reversing Array
 console.log("Array Sorted Asc-Dsc", Arr2.sort());  // Works only for Sorting Strings.
-console.log("Sorting Number Array: ", Arr2.sort(function (x, y){ return x-y }));   // This is Logic Needed To Sort Numberical Arrays. Function Compares 2 Values and Sorts them Accordingly. Use y - x for Reverse Sorting from Dsc-Asc.
+console.log("Sorting Number Array: ", Arr2.sort(function (x, y) { return x - y }));   // This is Logic Needed To Sort Numberical Arrays. Function Compares 2 Values and Sorts them Accordingly. Use y - x for Reverse Sorting from Dsc-Asc.
 
 // Find Function Requires Function Declared Beforehand
-function condition(num){
-  return num%2==0;
-}   // This Logic Will Return First Even Number in Array.
+const condition = num => num % 2 === 0 /* Always use Strict === To Avoid Errors.*/   // This Logic Will Return First Even Number in Array. // Also The Way This Function is Declared is Called Arrow Function.
 console.log("Find Function For Even Number: ", Arr2.find(condition));   // Make Your Own Conditions using <>!== fibonacci etc.
---
+++ VanillaJS/Lesson 08: Date Functions/app.js
@@ -0,0 +1,31 @@
+// Date
+let now = new Date();                  //Declaration
+console.log(now);
+now = new Date(1988,3,4,5,6,0);        //Method 1
+console.log(now);
+now = new Date('1/4/2004');            //Method 2
+console.log(now);
+now = new Date('September 5 1988');    //Method 3
+console.log(now);
+
+// Date Functions // Index 0 = January = Sunday
+
+console.log("Year(Get): ", now.getFullYear());
+now.setFullYear(2021);
+console.log("Year(SET): ", now.getFullYear());  // FullYear
+
+console.log("Month(Get): ", now.getMonth());
+now.setMonth(1);
+console.log("Month(Set): ", now.getMonth());  // Month
+
+console.log("Day(Get): ", now.getDay());  // No Set Function
+
+console.log("Date(Get): ", now.getDate());
+now.setDate(30);
+console.log("Date(Set): ", now.getDate());    // Date
+
+console.log("Hour(Get): ", now.getHours());
+now.setHours(23);
+console.log("Hour(Set): ", now.getHours());   // Hours
+
+console.log("Minutes:(Get) ")
